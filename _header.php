9a6a2895911f5cd3b2314ee05738c3dcfcc2c03b
mawr <mawr@mail.ru>
2021-09-16 15:47:02 +0300

    Страница "Товары"

--- _header.php
+++ _header.php
@@ -2,7 +2,7 @@
     <div class="wrapper">
       <a href="/" class="logo"><img src="/img/icon-192x192.png" /><div>Рубин</div></a>
       <ul class="scrollable unselectable">
-        <li><a href="#">Продукция</a></li>
+        <li><a href="/goods/">Продукция</a></li>
         <li><a href="#">Где купить</a></li>
         <li><a href="#">Контакты</a></li>
       </ul>
---
+++ goods.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="ru-RU">
+<head>
+  <title>Продукция Рубин - аккумуляторы, батарейки, официальный сайт</title>
+  <meta name="description" content="Аккумуляторы и батарейки Рубин. Официальный сайт." />
+  <link rel="canonical" href="https://rubin-electro.ru/" />
+
+  <?php include('_meta.php'); ?>
+
+  <link rel="stylesheet" href="/css/goods.css" media="all" />
+
+</head>
+<body>
+  <?php include('_header.php'); ?>
+
+  <div class="wrapper content">
+
+    <h1>Продукция Рубин</h1>
+
+    <div class="goods-list">
+      <a href="/goods/prod-1.htm" class="product">
+        <img src="/img/goods/battery-green-aa/01.jpg">
+        <div>
+           Аккумулятор <strong>Рубин АА USB</strong>
+           <div class="gray-text">
+             Li-ion аккумулятор типоразмера AA со встроенной USB-зарядкой
+           </div>
+        </div>
+      </a>
+      <a href="/goods/prod-1.htm" class="product">
+        <img src="/img/goods/battery-red-aa/01.jpg">
+        <div>
+           Аккумулятор <strong>Рубин АА USB-F</strong>
+           <div class="gray-text">
+             Li-ion аккумулятор типоразмера AA со встроенной USB-зарядкой
+           </div>
+        </div>
+      </a>
+      <a href="/goods/prod-1.htm" class="product">
+        <img src="/img/goods/battery-red-aaa/01.jpg">
+        <div>
+           Аккумулятор <strong>Рубин ААA USB-F</strong>
+           <div class="gray-text">
+             Li-ion аккумулятор типоразмера AA со встроенной USB-зарядкой
+           </div>
+        </div>
+      </a>
+    </div>
+
+  </div>
+
+  <?php include('_footer.php'); ?>
+</body>
+</html>
